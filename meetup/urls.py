2f20162e65df3acba5281b95344ee24ec03f47aa
Bradley Stuart Kirton <bradleykirton@gmail.com>
2018-06-30 13:03:16 +0200

    Replaced the default router with a simple router and a custom root api view which includes the graphql-drf view.

--- meetup/urls.py
+++ meetup/urls.py
@@ -2,17 +2,34 @@ import django.urls
 import meetup.views
 import meetup.schema
 import meetup.views
+import rest_framework.reverse
+import rest_framework.request
+import rest_framework.response
+import rest_framework.views
 import rest_framework.routers
 import graphene
 import graphene_django.views
 
 
 # Setup the DRF routes
-router = rest_framework.routers.DefaultRouter()
-router.register('meetups', meetup.views.MeetupViewSet)
-router.register('events', meetup.views.EventViewSet)
-router.register('attendees', meetup.views.AttendeeViewSet)
-router.register('attendance', meetup.views.AttendanceViewSet)
+router = rest_framework.routers.SimpleRouter()
+router.register('meetups', meetup.views.MeetupViewSet, base_name='meetup')
+router.register('events', meetup.views.EventViewSet, base_name='event')
+router.register('attendees', meetup.views.AttendeeViewSet, base_name='attendee')
+router.register('attendance', meetup.views.AttendanceViewSet, base_name='attendance')
+
+
+class RootApiView(rest_framework.views.APIView):
+	def get(self, request: rest_framework.request.Request) -> rest_framework.response.Response:
+		routes = {
+			'meetups': rest_framework.reverse.reverse('meetup-list', request=request),
+			'events': rest_framework.reverse.reverse('event-list', request=request),
+			'attendees': rest_framework.reverse.reverse('attendee-list', request=request),
+			'attendance': rest_framework.reverse.reverse('attendance-list', request=request),
+			'graphql-drf': rest_framework.reverse.reverse('graphql-drf', request=request)
+		}
+
+		return rest_framework.response.Response(routes)
 
 
 # Create the graphql view (No integration with DRF)
@@ -25,8 +42,9 @@ drf_graphql_view = meetup.views.RestGraphQL.as_view(schema=schema)
 
 
 urlpatterns = [
-	django.urls.path('tasks/', meetup.views.Task.as_view(), name='tasks-page'),
 	django.urls.path('', graphql_view),
 	django.urls.path('api/', django.urls.include(router.urls)),
-	django.urls.path('api/graphql/', drf_graphql_view)
+	django.urls.path('api/', RootApiView.as_view()),
+	django.urls.path('api/graphql/', drf_graphql_view, name='graphql-drf'),
+	django.urls.path('tasks/', meetup.views.Task.as_view(), name='tasks-page')
 ]
